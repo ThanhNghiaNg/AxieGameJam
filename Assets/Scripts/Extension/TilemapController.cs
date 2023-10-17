c79108cdfa93b0dca06bbfb9397b624ce8cf7139
javthanhnghia <nghiant089@gmail.com>
2023-10-17 17:31:33 +0700

    add piece of genmap

--- Assets/Scripts/Extension/TilemapController.cs
+++ Assets/Scripts/Extension/TilemapController.cs
@@ -9,8 +9,26 @@ public class TilemapController : MonoBehaviour
     void Start()
     {
         tilemap = GetComponent<Tilemap>();
-        Vector3Int tilePosition = new Vector3Int(0, 0, 0);  // Set the position where you want to paint the tile
+        Vector3Int tilePosition = new Vector3Int(0, 0,0);  // Set the position where you want to paint the tile
         TileBase tile = tileEmpty;  // Reference to the tile you want to paint
         tilemap.SetTile(tilePosition, tile);
+        tilemap.SetTile(new Vector3Int(0, 1,0), tile);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = tilemap.WorldToCell(mousePosition);
+
+            // Check if the clicked position contains the specific tile.
+            if (tilemap.GetTile(gridPosition) == tileEmpty)
+            {
+                // Perform your action when the specific tile is clicked.
+                Debug.Log("Position of clicked tile: " + gridPosition);
+                Debug.Log("Do something");
+            }
+        }
     }
 }
