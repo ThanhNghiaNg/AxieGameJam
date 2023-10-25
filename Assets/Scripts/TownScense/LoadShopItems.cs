9bfc78b4780667ff8a2cfaac0d1f485bf0e88754
ThanhNghiaNg <nghiant089@gmail.com>
2023-10-25 23:22:21 +0700

    upload

--- Assets/Scripts/TownScense/LoadShopItems.cs
+++ Assets/Scripts/TownScense/LoadShopItems.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LoadShopItems : MonoBehaviour
 {
@@ -27,22 +30,28 @@ public class LoadShopItems : MonoBehaviour
     {
 
     }
-    public void CreateItemButton(int positionIndex)
+    public void CreateItemButton(int itemCost, Sprite itemSprite, string itemName, int positionIndex)
     {
         Transform shopItemTransform = Instantiate(shopItemTemplate, container);
         RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
 
-        float shopItemHeight = 30f;
+        float shopItemHeight = 68f;
         shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+        shopItemRectTransform.Find("ItemName").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemRectTransform.Find("ItemCost").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+        shopItemRectTransform.Find("ItemImage").GetComponent<Image>().sprite = itemSprite;
     }
     public void DisplayShopItems(Canvas canvas)
     {
         Debug.Log("lmao");
         Debug.Log(container);
         Debug.Log(shopItemTemplate);
-        CreateItemButton(0);
-        CreateItemButton(1);
-        CreateItemButton(2);
+        Debug.Log($"count: {items.Count}");
+        for (int i = 0; i < items.Count; i++)
+        {
+            CreateItemButton(items[i].price, items[i].sprite, items[i].itemName, i);
+        }
+
     }
 
 }
